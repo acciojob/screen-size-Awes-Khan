df7264ebf5076185a673ae7c6c15fce6f0878064
Awes Khan <44608315+Awes-Khan@users.noreply.github.com>
2024-08-26 23:53:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function updateScreenSize() {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    document.querySelector('#sizeInfo h1').textContent = `Width: ${width}, Height: ${height}`;
+}
+window.addEventListener('resize', updateScreenSize);
+updateScreenSize();
